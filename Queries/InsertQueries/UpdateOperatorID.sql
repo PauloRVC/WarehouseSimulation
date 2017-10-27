23606dbf25a00c8ce6bb666e6a37cf82fa38fb35
Daniel Ulch <dn549348@dal.ca>
2017-10-27 08:27:26 -0400

    Added Operator Query

---
+++ Queries/InsertQueries/UpdateOperatorID.sql
@@ -0,0 +1,35 @@
+
+UPDATE  OrderItems 
+SET OrderItems.OperatorID = bot.OperatorID
+
+FROM
+(
+SELECT 
+
+a.OrderID, a.ItemID, a.putTime, ops.OperatorID
+FROM
+(
+SELECT
+o.OrderID, i.ItemID, MAX(m.EventTime) AS putTime
+FROM 
+Orders AS o INNER JOIN
+MissionDetailHistory AS m ON o.OrderIndicator = m.TaskID
+INNER JOIN Items AS i ON i.ItemIndicator = m.SKUID
+INNER JOIN [Batches] AS b ON b.BatchID = o.BatchID
+WHERE o.OrderIndicator != b.BatchIndicator
+AND WorkAreaID = 'PutWall'
+AND [Event] = 'COMPLETED'
+GROUP BY o.OrderID, i.ItemID
+) 
+
+AS a 
+INNER JOIN Items AS it ON a.ItemID = it.ItemID
+INNER JOIN Orders AS ord ON a.OrderID = ord.OrderID
+INNER JOIN MissionDetailHistory AS h ON
+ord.OrderIndicator = h.TaskID
+AND it.ItemIndicator = h.SKUID
+AND a.putTime = h.EventTime
+LEFT OUTER JOIN Operators AS ops ON h.OperatorID = ops.OperatorID_Text
+) AS bot
+
+WHERE OrderItems.OrderID = bot.OrderID AND OrderItems.ItemID = bot.ItemID
